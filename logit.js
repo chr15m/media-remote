53a4d8afdf772ab543dfe7adda5d27d185d5bd6f
Chris McCormick <chris@mccormick.cx>
2014-12-12 21:11:41 +0800

    Added script to help build a logfile of what is being watched.

---
+++ logit.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env nodejs
+
+var mr = require('./mediaremote');
+
+mr.findTVs(function(tv) {
+	// console.log("Got TV:", tv.info);
+	tv.register(function(err, result) {
+		if (!err) {
+			// console.log("Successfully registered.");
+			tv.getStatus(function(err, statuses) {
+				if (err) {
+					console.log("error\tstatus-failed");
+				} else {
+					for (var s=0; s<statuses.length; s++) {
+						var output = [
+							tv.info["device"],
+							statuses[s]["title"],
+							statuses[s]["source"],
+							statuses[s]["provider"]
+						];
+						console.log(output.join("\t"));
+					}
+				}
+			});
+		} else {
+			console.log("error\tregistration-failed");
+		}
+	});
+}, function(tvs) {
+	// console.log("TVs found:", tvs.length);
+});
